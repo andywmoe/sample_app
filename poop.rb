12c968e96fdf14086170b80af889826c580c5737
Andy <andywmoe@gmail.com>
2011-02-02 01:45:33 -0800

    Finished layout and routes

---
+++ poop.rb
@@ -0,0 +1,5 @@
+person1 = { :first => "Bill", :last => "Moe" }
+person2 = { :first => "Shelly", :last => "Moe" }
+person3 = { :first => "Emily", :last => "Moe" }
+
+params = { :father => person1, :mother => person2, :child => person3 }
